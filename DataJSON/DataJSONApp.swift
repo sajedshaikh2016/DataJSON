bec8703e106c45d9f5f5dd0e840186775e5df463
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-10 17:28:00 +0530

    Initial Commit

---
+++ DataJSON/DataJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataJSONApp.swift
+//  DataJSON
+//
+//  Created by Sajed Shaikh on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DataJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
